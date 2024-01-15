440c4124c3540a5bd148b506838ffb62a0ac434b
Daniel Cazarez <cazarez.jorge.prog2019@gmail.com>
2024-01-15 12:50:28 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Create chrome extension CLI tool
+
+CLI tool to create a project for a chrome extension with typescript and tailwindcss.
+
+## Getting Started
+
+Link the package to your npm global folder:
+
+```bash
+npm link
+```
+
+How to run:
+
+```bash
+npm create chrome-extension
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+import { createInterface } from "readline";
+import { execSync } from "child_process";
+import { writeFileSync, readFileSync } from "fs";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let projectName = "";
+rl.question("Project name: ", (input) => {
+  if (!input.match(/^[a-z0-9-]+$/)) {
+    console.error("Invalid project name");
+    process.exit(1);
+  }
+
+  projectName = input;
+
+  console.log("Cloning template...");
+
+  execSync(
+    `git clone https://github.com/DanielCaz/chrome-extension-template.git ${projectName}`
+  );
+
+  const packageJson = JSON.parse(
+    readFileSync(`${projectName}/package.json`, "utf8")
+  );
+
+  packageJson.name = projectName;
+  delete packageJson.repository;
+  delete packageJson.bugs;
+  delete packageJson.homepage;
+  delete packageJson.author;
+  delete packageJson.license;
+
+  writeFileSync(
+    `${projectName}/package.json`,
+    JSON.stringify(packageJson, null, 2)
+  );
+
+  execSync(`rmdir /s /q ${projectName}\\.git`);
+
+  console.log("Installing dependencies...");
+  execSync(`cd ${projectName} && npm install`);
+
+  rl.close();
+});
+
+rl.on("close", () => {
+  console.log("Done!");
+
+  process.exit(0);
+});
